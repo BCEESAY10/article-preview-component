2720dd4bfa67e07e992b3a1519a51ec5eac7b527
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-11 00:16:21 +0000

    implement toggles

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // The Js code
 const shareBtn = document.getElementById("share");
 const socials = document.querySelector(".social");
+const triangle = document.querySelector(".triangle");
 
 shareBtn.addEventListener('click', () => {
     socials.classList.toggle("hide");
+    triangle.classList.toggle("hide");
 })
