09d6bdcd57f5150add166fab2426f38bb829f9a6
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-11 00:08:01 +0000

    toggle socials

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // The Js code
 const shareBtn = document.getElementById("share");
+const socials = document.querySelector(".social");
 
 shareBtn.addEventListener('click', () => {
-    alert("Yes");
+    socials.classList.toggle("hide");
 })
